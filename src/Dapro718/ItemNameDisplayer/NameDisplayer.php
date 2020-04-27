a303435d50dd723812860c522d9e39b980918eba
josephlopezh888 <letodamian1128@gmail.com>
2020-04-27 18:10:06 -0400

    Update NameDisplayer.php

--- src/Dapro718/ItemNameDisplayer/NameDisplayer.php
+++ src/Dapro718/ItemNameDisplayer/NameDisplayer.php
@@ -6,6 +6,26 @@ namespace Dapro718\ItemNameDisplayer;
 
 use pocketmine\event\Listener;
 use pocketmine\item\Item;
+use pocketmine\event
 use pocketmine\utils\Config
 
-class
+class NameDisplayer implements Listener {
+  
+  public $config;
+  public $plugin;
+  
+  public function __construct(Main $plugin) {
+    $this->plugin = $plugin;
+    $this->config = $plugin->getConfig();
+  }
+  
+  public function onItemDrop(ItemSpawnEvent $event) {
+    $item = $event->getItem();
+    $id = $item->getId();
+    $disabledItems = $this->config->get("disabled-items");
+    if(!in_array($id, $disabledItems, TRUE)) {
+      $displayName = $this->plugin->setItemDisplayName($item);
+      $this->plugin->setItemDisplay();
+    }
+  }
+}
