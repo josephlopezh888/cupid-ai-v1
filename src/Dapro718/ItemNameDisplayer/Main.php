3f39e7a027127bffc2c0d3c4835bb564963a4b21
josephlopezh888 <letodamian1128@gmail.com>
2020-04-27 19:29:18 -0400

    Update Main.php

--- src/Dapro718/ItemNameDisplayer/Main.php
+++ src/Dapro718/ItemNameDisplayer/Main.php
@@ -23,7 +23,6 @@ class Main extends PluginBase {
       $format = $this->config->get("display-format");
       $displayName = str_replace("{name}", $name, $format);
       return $displayName;
-    }
   }
   
   public function displayName($item, $displayName) {
