937f35099f0781bafb021931d2c18410d56b6a17
josephlopezh888 <letodamian1128@gmail.com>
2020-04-27 18:12:04 -0400

    Update Main.php

--- src/Dapro718/ItemNameDisplayer/Main.php
+++ src/Dapro718/ItemNameDisplayer/Main.php
@@ -5,7 +5,42 @@ declare(strict_types=1);
 namespace Dapro718\ItemNameDisplayer;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\item\Item;
+use pocketmine\utils\Config;
 
 
 class Main extends PluginBase {
+  
+  public $config;
+  
+  public function onEnable() void: {
+    $this->config = $this->getConfig();
+    $this->getServer()->getPluginManager->registerEvents(new NameDisplayer($this), $this);
+  }
+  
+  public function setItemDisplayName($item) {
+    if($item->hasCustomName()) {
+      $name = $item->getCustomName();
+      $format = $this->config->get("display-format");
+      $displayName = str_replace("{name}", $name, $format);
+      return $displayName;
+    } else {
+      $name = $item->getNameTag();
+      $format = $this->config->get("display-format");
+      $displayName = str_replace("{name}", $name, $format);
+      return $displayName;
+    }
+  }
+  
+  public function displayName($item, $displayName) {
+    $item->setNameTagVisible(true);
+    $item->setNameTagAlwaysVisible(true);
+    $item->setNameTag($displayName);
+  }
+}
+
+    
+      
+    
+    
 
