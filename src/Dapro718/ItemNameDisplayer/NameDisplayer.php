3e225fe48a308c903b90669653292c0d106184a5
josephlopezh888 <letodamian1128@gmail.com>
2020-04-30 12:03:01 -0400

    Added disabled worlds support

--- src/Dapro718/ItemNameDisplayer/NameDisplayer.php
+++ src/Dapro718/ItemNameDisplayer/NameDisplayer.php
@@ -23,12 +23,23 @@ class NameDisplayer implements Listener {
   
   public function onItemDrop(ItemSpawnEvent $event) {
     $entity = $event->getEntity();
+    $level = $entity->getLevel();
     $item = $entity->getItem();
+    $id = $item->getID();
+    $disabledWorlds = $this->config->get("disabled-worlds");
+    $disabledItems = $this->config->get("disabled-items");
     $name = $item->getName();
     $fcon = $this->config->get("display-format");
-    $format = str_replace("{name}", $name, $fcon);
-    $entity->setNameTag($format);
-    $entity->setNameTagVisible(true);
-    $entity->setNameTagAlwaysVisible(true);
+    if(in_array($level, $disabledWorlds, TRUE)) {
+      $event->setCancelled();
+      if(in_array($id, $disabledItems, TRUE)) {
+        $event->setCancelled();
+      } else {
+        $format = str_replace("{name}", $name, $fcon);
+        $entity->setNameTag($format);
+        $entity->setNameTagVisible(true);
+        $entity->setNameTagAlwaysVisible(true);
+      }
+    }
   }
 }
