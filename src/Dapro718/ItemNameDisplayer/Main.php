1693fdd9e9e575b3c9c235badd969d4775c767b8
josephlopezh888 <letodamian1128@gmail.com>
2020-04-30 11:53:05 -0400

    Update Main.php

--- src/Dapro718/ItemNameDisplayer/Main.php
+++ src/Dapro718/ItemNameDisplayer/Main.php
@@ -15,9 +15,20 @@ use pocketmine\utils\Config;
 class Main extends PluginBase {
   
   public $config;
+  private const CONFIG_VERSION = 2;
   
   public function onEnable() {
     $this->config = $this->getConfig();
     $this->getServer()->getPluginManager()->registerEvents(new NameDisplayer($this), $this);
+    $this->checkConfig();
+  }
+  
+  public function checkConfig() {
+    if ((!$this->getConfig()->exists("config-version")) || ($this->getConfig()->get("config-version") != self::CONFIG_VERSION)) {
+      rename($this->getDataFolder() . "config.yml", $this->getDataFolder() . "config_old.yml");
+      $this->saveResource("config.yml");
+      $this->getLogger()->critical("Your configuration file is outdated.");
+      $this->getLogger()->notice("Your old configuration has been saved as config_old.yml and a new configuration file has been generated. Please update accordingly.");
+    }
   }
 }
